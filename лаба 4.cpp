5639cbbebfae8cb73bed4d504da66ee611973a05
ssiffaaaa <135047893+ssiffaaaa@users.noreply.github.com>
2023-06-01 15:48:49 +0300

    Пояснение (4)
    код реализует базовый класс Square и производный классCube. Класс Square имеет одно защищенное поле - side, которое представляет длину стороны квадрата. Класс также имеет конструктор для инициализации side и метод area() для вычисления площади квадрата. Метод print() печатает side и area().
    
    Класс Cube является производным от Square и имеет один дополнительный метод volume(), который вычисляет объем куба. Класс также переопределяет метод area() для вычисления площади куба (6 * area() базового класса). Метод print() печатает side, area() и volume().
    
    В функции main() мы создаем объекты классов Square и Cube и вызываем методы print() для каждого объекта. Затем мы создаем указатель на объект класса Square, который указывает на объект класса Cube, и вызываем метод print(). Это демонстрирует динамический полиморфизм, так как метод print() вызывается для объекта класса Cube, но используется указатель на базовый класс Square.

---
+++ лаба 4.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+
+class Square {
+protected:
+    double side;
+
+public:
+    Square(double side = 0) {
+        this->side = side;
+    }
+
+    virtual double area() {
+        return side * side;
+    }
+
+    void print() {
+        std::cout << "Side: " << side << std::endl;
+        std::cout << "Area: " << area() << std::endl;
+    }
+};
+
+class Cube : public Square {
+public:
+    Cube(double side = 0) : Square(side) {}
+
+    virtual double area() {
+        return 6 * Square::area();
+    }
+
+    double volume() {
+        return side * side * side;
+    }
+
+    void print() {
+        std::cout << "Side: " << side << std::endl;
+        std::cout << "Area: " << area() << std::endl;
+        std::cout << "Volume: " << volume() << std::endl;
+    }
+};
+
+int main() {
+    Square s(5);
+    s.print();
+
+    Cube c(3);
+    c.print();
+
+    Square* ptr = &c;
+    ptr->print();
+
+    return 0;
+}
