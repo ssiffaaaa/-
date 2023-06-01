b55eae8154a17b6646ff4d9a7200f06917379600
ssiffaaaa <135047893+ssiffaaaa@users.noreply.github.com>
2023-06-01 15:58:49 +0300

    Пояснение (1)
    Код определяет класс Double, который представляет собой значение с плавающей точкой двойной точности. Класс содержит приватное поле value, которое хранит текущее значение, и публичные методы для инициализации, чтения, отображения, изменения и возведения в степень этого значения. В функции main создается объект класса Double, инициализируется его значение и отображается на экране. Затем значение изменяется с помощью методов add и subtract, и результаты отображаются на экране. Значение возведенное в степень отображается на экране с помощью метода power.

---
+++ лаба1.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+class Double {
+private:
+    double value;
+
+public:
+    void init() {
+        value = 0.0;
+    }
+
+    void read() {
+        cout << "Введите двойное значение: ";
+        cin >> value;
+    }
+
+    void display() {
+        cout << "Значение: " << value << endl;
+    }
+
+    void add(double num) {
+        value += num;
+    }
+
+    void subtract(double num) {
+        value -= num;
+    }
+
+    double power(int exponent) {
+        return pow(value, exponent);
+    }
+};
+
+int main() {
+    setlocale(0, "Ru");
+    Double d;
+    d.init();
+    d.read();
+    d.display();
+
+    cout << "Добавлениe 2.5 к значению..." << endl;
+    d.add(2.5);
+    d.display();
+
+    cout << "Вычитание 1.0 от значения..." << endl;
+    d.subtract(1.0);
+    d.display();
+
+    cout << "Значение возведено в степень 3: " << d.power(3) << endl;
+
+    return 0;
+}
